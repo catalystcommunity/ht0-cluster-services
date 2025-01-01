9a293405562b0dc96e1ba49e6930be2c7dc285be
Tod Hansmann <githubpub@todandlorna.com>
2025-01-01 16:51:25 -0700

    fix: make CI do something more than echo

--- ci/reactorcide-job.sh
+++ ci/reactorcide-job.sh
@@ -1,3 +1,20 @@
 #!/usr/bin/env bash
 
 echo "I am in CI in the innner root job script"
+set -x
+
+mkdir -p .kube
+touch .kube/config
+echo "home.todandlorna.com"
+# echo "${CI_KUBECONFIG}" > .kube/config
+# kubectl get pods -A
+
+# kubectl apply ./argocd-app.yaml
+
+# helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+# helm repo add catalyst-helm https://raw.githubusercontent.com/catalystcommunity/charts/main
+# helm repo update
+
+# helm upgrade --install -f ./helm_values/prometheus.yaml prometheus-community/kube-prometheus-stack --version 67.5.0
+
+# helm upgrade --install -f ./helm_values/cluster-services.yaml catalyst-helm/platform-services --version 2.1.1
