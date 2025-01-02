d7c1d7812fdbf1edabcddce82359a201192ea436
Tod Hansmann <githubpub@todandlorna.com>
2025-01-01 17:01:02 -0700

    fix: try everything helm related

--- ci/reactorcide-job.sh
+++ ci/reactorcide-job.sh
@@ -5,16 +5,15 @@ set -x
 
 mkdir -p .kube
 touch .kube/config
-echo "home.todandlorna.com"
-# echo "${CI_KUBECONFIG}" > .kube/config
-# kubectl get pods -A
+echo "${CI_KUBECONFIG}" > .kube/config
+kubectl get pods -A
 
-# kubectl apply ./argocd-app.yaml
+kubectl apply ./argocd-app.yaml
 
-# helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-# helm repo add catalyst-helm https://raw.githubusercontent.com/catalystcommunity/charts/main
-# helm repo update
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo add catalyst-helm https://raw.githubusercontent.com/catalystcommunity/charts/main
+helm repo update
 
-# helm upgrade --install -f ./helm_values/prometheus.yaml prometheus-community/kube-prometheus-stack --version 67.5.0
+helm upgrade --install -f ./helm_values/prometheus.yaml prometheus-community/kube-prometheus-stack --version 67.5.0
 
-# helm upgrade --install -f ./helm_values/cluster-services.yaml catalyst-helm/platform-services --version 2.1.1
+helm upgrade --install -f ./helm_values/cluster-services.yaml catalyst-helm/platform-services --version 2.1.1
