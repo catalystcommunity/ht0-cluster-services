598a88991f1751814705f3f78e09fe4ed7d38ef4
Tod Hansmann <githubpub@todandlorna.com>
2025-01-01 17:08:28 -0700

    fix: use reactorcide vars like we are supposed to

--- ci/reactorcide-job.sh
+++ ci/reactorcide-job.sh
@@ -8,12 +8,12 @@ touch .kube/config
 echo "${CI_KUBECONFIG}" > .kube/config
 kubectl get pods -A
 
-kubectl apply -f ./argocd-app.yaml
+kubectl apply -f ${REACTORCIDE_REPOROOT}/argocd-app.yaml
 
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo add catalyst-helm https://raw.githubusercontent.com/catalystcommunity/charts/main
 helm repo update
 
-helm upgrade --install prometheus -f ./helm_values/prometheus.yaml prometheus-community/kube-prometheus-stack --version 67.5.0
+helm upgrade --install prometheus -f ${REACTORCIDE_REPOROOT}/helm_values/prometheus.yaml prometheus-community/kube-prometheus-stack --version 67.5.0
 
-helm upgrade --install cluster-services -f ./helm_values/cluster-services.yaml catalyst-helm/platform-services --version 2.1.1
+helm upgrade --install cluster-services -f ${REACTORCIDE_REPOROOT}/helm_values/cluster-services.yaml catalyst-helm/platform-services --version 2.1.1
