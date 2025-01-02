a36656d83e71178ffe36a002dcab188366b36696
Tod Hansmann <githubpub@todandlorna.com>
2025-01-01 17:31:26 -0700

    fix: move sed to actual ci job, not gha release script

--- ci/reactorcide-job.sh
+++ ci/reactorcide-job.sh
@@ -3,10 +3,23 @@
 echo "I am in CI in the innner root job script"
 set -x
 
+CLUSTER_SERVICES_FILE="${REACTORCIDE_REPOROOT}/helm_values/cluster-services.yaml"
+PROMETHEUS_FILE="${REACTORCIDE_REPOROOT}/helm_values/prometheus.yaml"
+
+sed -i 's/{{cloudflareApiEmail}}/${CLOUDFLARE_API_EMAIL}/g' ${CLUSTER_SERVICES_FILE}
+sed -i 's/{{cloudflareApiToken}}/${CLOUDFLARE_API_TOKEN}/g' ${CLUSTER_SERVICES_FILE}
+sed -i 's/{{grafanaDatasourceCortexPassword}}/${GRAFANA_CORTEXT_PASSWORD}/g' ${CLUSTER_SERVICES_FILE}
+sed -i 's/{{grafanaDatasourceLokiPassword}}/${GRAFANA_LOKI_PASSWORD}/g' ${CLUSTER_SERVICES_FILE}
+sed -i 's/{{grafanaAdminPassword}}/${GRAFANA_ADMIN_PASSWORD}/g' ${CLUSTER_SERVICES_FILE}
+sed -i 's/{{grafanaNotifierCatalystCommunityAlerts}}/${CATALYST_COMMUNITY_ALERTS_URL}/g' ${CLUSTER_SERVICES_FILE}
+sed -i 's/{{linkerdIssuerKeyPEM}}/${LINKERD_ISSUER_KEY_PEM}/g' ${CLUSTER_SERVICES_FILE}
+sed -i 's/{{promtailBasicAuthPassword}}/${PROMTAIL_PASS}/g' ${CLUSTER_SERVICES_FILE}
+
+sed -i 's/{{clusterName}}/${CLUSTER_NAME}/g' ${PROMETHEUS_FILE}
+
 mkdir -p .kube
 touch .kube/config
 echo "${CI_KUBECONFIG}" > .kube/config
-kubectl get pods -A
 
 kubectl apply -f ${REACTORCIDE_REPOROOT}/argocd-app.yaml
 
@@ -14,6 +27,6 @@ helm repo add prometheus-community https://prometheus-community.github.io/helm-c
 helm repo add catalyst-helm https://raw.githubusercontent.com/catalystcommunity/charts/main
 helm repo update
 
-helm upgrade --install prometheus -f ${REACTORCIDE_REPOROOT}/helm_values/prometheus.yaml prometheus-community/kube-prometheus-stack --version 67.5.0
+helm upgrade --install prometheus -f ${PROMETHEUS_FILE} prometheus-community/kube-prometheus-stack --version 67.5.0
 
-helm upgrade --install cluster-services -f ${REACTORCIDE_REPOROOT}/helm_values/cluster-services.yaml catalyst-helm/platform-services --version 2.1.1
+helm upgrade --install cluster-services -f ${CLUSTER_SERVICES_FILE} catalyst-helm/platform-services --version 2.1.1
--- ci/release.sh
+++ ci/release.sh
@@ -1,26 +1,20 @@
 #!/usr/bin/env bash
 
-set -x
-CLUSTER_SERVICES_FILE="./helm_values/cluster-services.yaml"
-PROMETHEUS_FILE="./helm_values/prometheus.yaml"
-
-sed -i 's/{{cloudflareApiEmail}}/${CLOUDFLARE_API_EMAIL}/g' ${CLUSTER_SERVICES_FILE}
-sed -i 's/{{cloudflareApiToken}}/${CLOUDFLARE_API_TOKEN}/g' ${CLUSTER_SERVICES_FILE}
-sed -i 's/{{grafanaDatasourceCortexPassword}}/${GRAFANA_CORTEXT_PASSWORD}/g' ${CLUSTER_SERVICES_FILE}
-sed -i 's/{{grafanaDatasourceLokiPassword}}/${GRAFANA_LOKI_PASSWORD}/g' ${CLUSTER_SERVICES_FILE}
-sed -i 's/{{grafanaAdminPassword}}/${GRAFANA_ADMIN_PASSWORD}/g' ${CLUSTER_SERVICES_FILE}
-sed -i 's/{{grafanaNotifierCatalystCommunityAlerts}}/${CATALYST_COMMUNITY_ALERTS_URL}/g' ${CLUSTER_SERVICES_FILE}
-sed -i 's/{{linkerdIssuerKeyPEM}}/${LINKERD_ISSUER_KEY_PEM}/g' ${CLUSTER_SERVICES_FILE}
-sed -i 's/{{promtailBasicAuthPassword}}/${PROMTAIL_PASS}/g' ${CLUSTER_SERVICES_FILE}
-
-sed -i 's/{{clusterName}}/${CLUSTER_NAME}/g' ${PROMETHEUS_FILE}
-
 touch runnerenv.sh
 echo "REACTORCIDE_JOB_REPO_URL=${REACTORCIDE_JOB_REPO_URL}" >> runnerenv.sh
 
 touch jobenv.sh
 echo "REACTORCIDE_JOB_ENTRYPOINT=${REACTORCIDE_JOB_ENTRYPOINT}" >> jobenv.sh
 echo "CI_KUBECONFIG=\"${CI_KUBECONFIG}\"" >> jobenv.sh
+echo "CLOUDFLARE_API_EMAIL=\"${CLOUDFLARE_API_EMAIL}\"" >> jobenv.sh
+echo "CLOUDFLARE_API_TOKEN=\"${CLOUDFLARE_API_TOKEN}\"" >> jobenv.sh
+echo "GRAFANA_CORTEXT_PASSWORD=\"${GRAFANA_CORTEXT_PASSWORD}\"" >> jobenv.sh
+echo "GRAFANA_LOKI_PASSWORD=\"${GRAFANA_LOKI_PASSWORD}\"" >> jobenv.sh
+echo "GRAFANA_ADMIN_PASSWORD=\"${GRAFANA_ADMIN_PASSWORD}\"" >> jobenv.sh
+echo "CATALYST_COMMUNITY_ALERTS_URL=\"${CATALYST_COMMUNITY_ALERTS_URL}\"" >> jobenv.sh
+echo "LINKERD_ISSUER_KEY_PEM=\"${LINKERD_ISSUER_KEY_PEM}\"" >> jobenv.sh
+echo "PROMTAIL_PASS=\"${PROMTAIL_PASS}\"" >> jobenv.sh
+echo "CLUSTER_NAME=\"${CLUSTER_NAME}\"" >> jobenv.sh
 
 touch cisshkey
 echo "${CI_SSH_KEY}" >> cisshkey
