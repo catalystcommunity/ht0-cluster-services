61e173fd450664c6c0e42dc3ec82db4bb89e94fd
Tod Hansmann <githubpub@todandlorna.com>
2025-01-05 18:13:51 -0700

    fix: use uv and a python script for the complicated replacements

---
+++ ci/ci_replacements.py
@@ -0,0 +1,87 @@
+import yaml
+import sys
+import os
+
+# File paths we'll be working with
+PLATFORM_SERVICES_TEMPLATE=os.environ.get("PLATFORM_SERVICES_TEMPLATE")
+PLATFORM_SERVICES_FILE=os.environ.get("PLATFORM_SERVICES_FILE")
+PROMETHEUS_TEMPLATE=os.environ.get("PROMETHEUS_TEMPLATE")
+PROMETHEUS_FILE=os.environ.get("PROMETHEUS_FILE")
+
+# The replacements we'll be making
+PLATFORM_REPLACEMENTS = {
+    'cloudflareApiEmail': os.environ.get('CLOUDFLARE_API_EMAIL'),
+    'cloudflareApiToken': os.environ.get('CLOUDFLARE_API_TOKEN'),
+    'grafanaDatasourceCortexPassword': os.environ.get('GRAFANA_CORTEXT_PASSWORD'),
+    'grafanaDatasourceLokiPassword': os.environ.get('GRAFANA_LOKI_PASSWORD'),
+    'grafanaAdminPassword': os.environ.get('GRAFANA_ADMIN_PASSWORD'),
+    'grafanaNotifierCatalystCommunityAlerts': os.environ.get('CATALYST_COMMUNITY_ALERTS_URL'),
+    'linkerdIssuerKeyPEM': os.environ.get('LINKERD_ISSUER_KEY_PEM'),
+    'promtailBasicAuthPassword': os.environ.get('PROMTAIL_PASS'),
+}
+PROMETHEUS_REPLACEMENTS = {
+    'clusterName': os.environ.get('CLUSTER_NAME'),
+}
+
+
+def replace_value_in_file(input_file_path, output_file_path, replacement_dict):
+    with open(input_file_path, "r") as file:
+        lines = file.readlines()
+
+    with open(output_file_path, "w") as file:
+        for line in lines:
+            for key, value in replacement_dict.items():
+                if key in line.strip():
+                    indent = line[: len(line) - len(line.lstrip())]
+                    new_lines = [indent + new_line for new_line in value.split("\n")]
+                    line = "\n".join(new_lines) + "\n"
+            file.write(line)
+
+def validate_yaml(file_path):
+    try:
+        with open(file_path, "r") as file:
+            yaml.safe_load(file)
+        return True
+    except yaml.YAMLError:
+        return False
+
+def main():
+    validSetup = True
+    if None in PLATFORM_REPLACEMENTS.values() + PROMETHEUS_REPLACEMENTS.values():
+        print("Error: Missing environment variables.")
+        validSetup = False
+        for key, value in PLATFORM_REPLACEMENTS.items() + PROMETHEUS_REPLACEMENTS.items():
+            if value is None:
+                print(f"Missing expected environent variable value for template key: {key}")
+    # This is just stupid and ugly, but we're doing it since this is just a CI script
+    if None in [PLATFORM_SERVICES_TEMPLATE, PLATFORM_SERVICES_FILE, PROMETHEUS_TEMPLATE, PROMETHEUS_FILE]:
+        print("Error: Missing environment variables.")
+        for key, value in {
+            "PLATFORM_SERVICES_TEMPLATE": PLATFORM_SERVICES_TEMPLATE,
+            "PLATFORM_SERVICES_FILE": PLATFORM_SERVICES_FILE,
+            "PROMETHEUS_TEMPLATE": PROMETHEUS_TEMPLATE,
+            "PROMETHEUS_FILE": PROMETHEUS_FILE,
+        }.items():
+            if value is None:
+                print(f"Missing value for {key}")
+                validSetup = False
+
+    if not validSetup:
+        sys.exit(1)
+
+    replace_value_in_file(PLATFORM_SERVICES_TEMPLATE, PLATFORM_SERVICES_FILE, PLATFORM_REPLACEMENTS)
+    replace_value_in_file(PROMETHEUS_TEMPLATE, PROMETHEUS_FILE, PROMETHEUS_REPLACEMENTS)
+
+    if not validate_yaml(PLATFORM_SERVICES_FILE):
+        print(
+            f"Error: The file {PLATFORM_SERVICES_FILE} is not valid YAML after replacements."
+        )
+        sys.exit(1)
+    if not validate_yaml(PROMETHEUS_FILE):
+        print(
+            f"Error: The file {PROMETHEUS_FILE} is not valid YAML after replacements."
+        )
+        sys.exit(1)
+
+if __name__ == "__main__":
+    main()
--- ci/reactorcide-job.sh
+++ ci/reactorcide-job.sh
@@ -3,19 +3,10 @@
 echo "I am in CI in the innner root job script"
 set -x
 
-PLATFORM_SERVICES_FILE="${REACTORCIDE_REPOROOT}/helm_values/platform-services.yaml"
-PROMETHEUS_FILE="${REACTORCIDE_REPOROOT}/helm_values/prometheus.yaml"
-
-sed -i 's/{{cloudflareApiEmail}}/${CLOUDFLARE_API_EMAIL}/g' ${PLATFORM_SERVICES_FILE}
-sed -i 's/{{cloudflareApiToken}}/${CLOUDFLARE_API_TOKEN}/g' ${PLATFORM_SERVICES_FILE}
-sed -i 's/{{grafanaDatasourceCortexPassword}}/${GRAFANA_CORTEXT_PASSWORD}/g' ${PLATFORM_SERVICES_FILE}
-sed -i 's/{{grafanaDatasourceLokiPassword}}/${GRAFANA_LOKI_PASSWORD}/g' ${PLATFORM_SERVICES_FILE}
-sed -i 's/{{grafanaAdminPassword}}/${GRAFANA_ADMIN_PASSWORD}/g' ${PLATFORM_SERVICES_FILE}
-sed -i 's/{{grafanaNotifierCatalystCommunityAlerts}}/${CATALYST_COMMUNITY_ALERTS_URL}/g' ${PLATFORM_SERVICES_FILE}
-sed -i 's/{{linkerdIssuerKeyPEM}}/${LINKERD_ISSUER_KEY_PEM}/g' ${PLATFORM_SERVICES_FILE}
-sed -i 's/{{promtailBasicAuthPassword}}/${PROMTAIL_PASS}/g' ${PLATFORM_SERVICES_FILE}
-
-sed -i 's/{{clusterName}}/${CLUSTER_NAME}/g' ${PROMETHEUS_FILE}
+PLATFORM_SERVICES_TEMPLATE="${REACTORCIDE_REPOROOT}/helm_values/platform-services.yaml"
+PLATFORM_SERVICES_FILE="${REACTORCIDE_REPOROOT}/helm_values/ciresult_platform-services.yaml"
+PROMETHEUS_TEMPLATE="${REACTORCIDE_REPOROOT}/helm_values/prometheus.yaml"
+PROMETHEUS_FILE="${REACTORCIDE_REPOROOT}/helm_values/ciresult_prometheus.yaml"
 
 mkdir -p .kube
 touch .kube/config
@@ -30,6 +21,22 @@ helm repo update
 # Install CRDs first
 kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.16.2/cert-manager.crds.yaml
 kubectl apply -f https://raw.githubusercontent.com/projectcontour/contour/v1.30.1/examples/contour/01-crds.yaml
+
+sed -i 's/{{cloudflareApiEmail}}/${CLOUDFLARE_API_EMAIL}/g' ${PLATFORM_SERVICES_FILE}
+sed -i 's/{{cloudflareApiToken}}/${CLOUDFLARE_API_TOKEN}/g' ${PLATFORM_SERVICES_FILE}
+sed -i 's/{{grafanaDatasourceCortexPassword}}/${GRAFANA_CORTEXT_PASSWORD}/g' ${PLATFORM_SERVICES_FILE}
+sed -i 's/{{grafanaDatasourceLokiPassword}}/${GRAFANA_LOKI_PASSWORD}/g' ${PLATFORM_SERVICES_FILE}
+sed -i 's/{{grafanaAdminPassword}}/${GRAFANA_ADMIN_PASSWORD}/g' ${PLATFORM_SERVICES_FILE}
+sed -i 's/{{grafanaNotifierCatalystCommunityAlerts}}/${CATALYST_COMMUNITY_ALERTS_URL}/g' ${PLATFORM_SERVICES_FILE}
+sed -i 's/{{linkerdIssuerKeyPEM}}/${LINKERD_ISSUER_KEY_PEM}/g' ${PLATFORM_SERVICES_FILE}
+sed -i 's/{{promtailBasicAuthPassword}}/${PROMTAIL_PASS}/g' ${PLATFORM_SERVICES_FILE}
+
+sed -i 's/{{clusterName}}/${CLUSTER_NAME}/g' ${PROMETHEUS_FILE}
+
+uv venv
+uv pip install pyyaml
+uv run python ${REACTORCIDE_REPOROOT}/ci/ci_replacements.py
+
 
 # Now Helm Chart
 helm upgrade --install --create-namespace --namespace kube-prometheus-stack prometheus -f ${PROMETHEUS_FILE} prometheus-community/kube-prometheus-stack --version 67.5.0
